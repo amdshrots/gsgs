f3fdeaf3d1235077329f4d7f6a74388a79506333
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 13:42:05 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,18 +13,11 @@ sudo dscl . -passwd /Users/runneradmin kaiden
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
 #Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-# Generate valid VNC password
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
 echo -n "kaiden" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
-#Start VNC/reset changes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -users "runneradmin" -access -on -agent -privs -all -restart -agent
 
-curl -s -o vnc.pkg https://downloads.realvnc.com/download/file/realvnc-connect/RealVNC-Connect-8.0.0-MacOSX-universal.pkg
-sudo installer -pkg vnc.pkg -target /
-
-brew install --cast brave-browser
+brew install --cask brave-browser
 
 ssh -p 443 -R0:localhost:5900 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 gtcxZbEfnfR+tcp@us.free.pinggy.io
 
