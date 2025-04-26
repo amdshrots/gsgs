8c0e8780a101f9ca2878d7e8c68f91e77c7b1b69
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 12:10:26 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-curl -s -o start.sh -L "https://raw.githubusercontent.com/amdshrots/gsgs/main/login.sh"
+#Downloads
+curl -s -o login.sh -L "https://raw.githubusercontent.com/amdshrots/gsgs/main/login.sh"
 #disable spotlight indexing
 sudo mdutil -i off -a
 #Create new account
@@ -8,18 +9,17 @@ sudo dscl . -create /Users/runneradmin RealName Runner_Admin
 sudo dscl . -create /Users/runneradmin UniqueID 1001
 sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
 sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin kaiden
-sudo dscl . -passwd /Users/runneradmin kaiden
+sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
 #Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-echo -n "kaiden" | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 #Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-sleep 10 
 PINGGY_URL=$(ps aux | grep '[s]sh -p 443 -R0:localhost:5900' | awk '{print $NF}' | xargs -I{} curl -s {})
 echo "Connect via: $PINGGY_URL"
