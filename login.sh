7ec0f16787261e036d376fb4dbe52d6dbde3bc04
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 12:09:17 -0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -3,4 +3,4 @@ echo ..........................................................
 echo IP:
 ssh -p 443 -R0:localhost:5900 -o StrictHostKeyChecking=no gtcxZbEfnfR+tcp@us.free.pinggy.io < /dev/null &
 echo Username: runneradmin
-echo Password: kaiden
+echo Password: P@ssw0rd!
