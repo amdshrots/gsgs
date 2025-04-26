53df02380ad546cc716e7ee8544d0cfa71c7297f
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 12:39:18 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ sudo dscl . -append /Groups/admin GroupMembership runneradmin
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 # Generate valid VNC password
-echo -n "kaiden" | sudo vncpasswd -s /Library/Preferences/com.apple.VNCSettings.txt
+echo -n "kaiden" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 #Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
