6fd43a61414730e501f20d318aef94d2f0ee9389
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 10:47:42 -0700

    Create login.sh

---
+++ login.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo ..........................................................
+echo IP:
+ssh -p 443 -R0:localhost:5900 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 gtcxZbEfnfR+tcp@us.free.pinggy.io
+echo Username: runneradmin
+echo Password: P@ssw0rd!
